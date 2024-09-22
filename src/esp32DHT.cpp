9289e8474cda6364ed3bcbe2062b627590db375b
timoxd7 <timoxd7@gmx.de>
2024-09-22 23:11:07 +0200

    Replacing rmt_set_pin with rmt_set_gpio

--- src/esp32DHT.cpp
+++ src/esp32DHT.cpp
@@ -117,8 +117,8 @@ void DHT::_readSensor(DHT* instance) {
     ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_set_direction((gpio_num_t)instance->_pin, GPIO_MODE_INPUT));
     rmt_rx_start(instance->_channel, 1);
     // rmt_set_pin is used untill platformio updates to latest Arduino core.
-    // rmt_set_gpio(instance->_channel, RMT_MODE_RX, static_cast<gpio_num_t>(instance->_pin), false);  // reset after using pin as output
-    rmt_set_pin(instance->_channel, RMT_MODE_RX, static_cast<gpio_num_t>(instance->_pin));  // reset after using pin as output
+    rmt_set_gpio(instance->_channel, RMT_MODE_RX, static_cast<gpio_num_t>(instance->_pin), false);  // reset after using pin as output
+    // rmt_set_pin(instance->_channel, RMT_MODE_RX, static_cast<gpio_num_t>(instance->_pin));  // reset after using pin as output
 
     // blocks until data is available or timeouts after 1000
     rmt_item32_t* items = static_cast<rmt_item32_t*>(xRingbufferReceive(instance->_ringBuf, &rx_size, 1000));
